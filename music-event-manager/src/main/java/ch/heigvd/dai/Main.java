3d4b2d99ce4b8fdb6ce4048661a8e6853b3580e8
Gianni <giannicecchetto59@gmail.com>
2025-01-06 15:51:50 +0100

    Started making api

--- music-event-manager/src/main/java/ch/heigvd/dai/Main.java
+++ music-event-manager/src/main/java/ch/heigvd/dai/Main.java
@@ -1,7 +1,13 @@
 package ch.heigvd.dai;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = Javalin.create();
+
+
+
+        app.post();
     }
 }
