529d4188a5ba65b2128b44217eecbd7c525d5c18
Gianni <giannicecchetto59@gmail.com>
2025-01-10 10:21:05 +0100

    Success in printing from a .jte file

---
+++ music-event-manager/jte-classes/gg/jte/generated/ondemand/JterootGenerated.java
@@ -0,0 +1,12 @@
+package gg.jte.generated.ondemand;
+@SuppressWarnings("unchecked")
+public final class JterootGenerated {
+	public static final String JTE_NAME = "root.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,0,0,0,0,0,0,0};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeContent("Hello jte!aasdsd");
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
--- music-event-manager/src/main/java/ch/heigvd/dai/Main.java
+++ music-event-manager/src/main/java/ch/heigvd/dai/Main.java
@@ -1,55 +1,15 @@
 package ch.heigvd.dai;
 
 import io.javalin.Javalin;
-import gg.jte.ContentType;
-import gg.jte.TemplateEngine;
-import gg.jte.TemplateOutput;
-import gg.jte.output.StringOutput;
-import gg.jte.resolve.DirectoryCodeResolver;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import io.javalin.rendering.template.JavalinJte;
 
-import java.sql.*;
-import java.util.Arrays;
-import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
+        Javalin app = Javalin.create(
+                config -> { config.fileRenderer(new JavalinJte()); }
+        ).start(7000);
 
-        final String url = "jdbc:postgresql://localhost/bdr:5432";
-        final Properties props = new Properties();
-        props.setProperty("user", "bdr");
-        props.setProperty("password", "bdr");
-        try (Connection conn = DriverManager.getConnection(url, props)) {
-            System.out.println(conn.getMetaData().getDatabaseProductVersion());
-        } catch(SQLException e) {
-            System.out.println("Error connecting to database " + Arrays.toString(e.getStackTrace()));
-        }
-
-        // Définir le répertoire contenant les fichiers JTE
-        Path templateDirectory = Path.of("templates");
-        DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(templateDirectory);
-
-        // Créer le moteur de template
-        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
-
-        // Démarrer Javalin
-        Javalin app = Javalin.create(config -> {
-        //config.staticFiles.add("public"); // Ajouter des fichiers statiques si nécessaire
-        }).start(7000);
-
-        // Route pour afficher une page HTML
-        app.get("/", ctx -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("message", "Bienvenue sur ma page dynamique avec Javalin et JTE !");
-
-            // Rendre le template
-            StringOutput output = new StringOutput();
-            templateEngine.render("index.jte", model, output);
-
-            // Renvoyer le contenu HTML
-            ctx.html(output.toString());
-        });
+        app.get("/", ctx -> ctx.render("root.jte"));
     }
 }
