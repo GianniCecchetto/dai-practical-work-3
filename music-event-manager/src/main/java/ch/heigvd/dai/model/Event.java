c5155148e1856c730b65ce40a606feb027689891
nathan.tschantz <nathan.tschantz@hes-so.ch>
2025-01-15 15:22:45 +0100

    added redirection for event details

--- music-event-manager/src/main/java/ch/heigvd/dai/model/Event.java
+++ music-event-manager/src/main/java/ch/heigvd/dai/model/Event.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public record Event(String name, Date beginning, Date end) {
+public record Event(String name, Date beginning, Date end,Integer id) {
     public static List<Event> getEvents(Connection connection) throws SQLException {
         if (connection == null || connection.isClosed()) {
             throw new SQLException("La connexion à la base de données est fermée ou non initialisée.");
         }
 
-        String sql = "SELECT DISTINCT nom, date_debut,date_fin FROM evenement ;";
+        String sql = "SELECT DISTINCT nom, date_debut,date_fin,id FROM evenement ;";
         List<Event> events = new ArrayList<>();
 
         try (Statement stmt = connection.createStatement()) {
@@ -20,7 +20,8 @@ public record Event(String name, Date beginning, Date end) {
                 events.add(new Event(
                         resultSet.getString("nom"),
                         resultSet.getDate("date_debut"),
-                        resultSet.getDate("date_fin")
+                        resultSet.getDate("date_fin"),
+                        resultSet.getInt("id")
                 ));
             }
         }
