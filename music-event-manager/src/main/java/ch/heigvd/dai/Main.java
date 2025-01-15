eb1a760440919dd61d0cfd5a64cfb13391c84611
Léonard Jouve <leonard.jouve@gmail.com>
2025-01-15 08:37:00 +0100

    benefits

--- music-event-manager/src/main/java/ch/heigvd/dai/Main.java
+++ music-event-manager/src/main/java/ch/heigvd/dai/Main.java
@@ -1,19 +1,21 @@
 package ch.heigvd.dai;
 
+import ch.heigvd.dai.controller.BenefitsController;
 import ch.heigvd.dai.controller.UsersController;
-import ch.heigvd.dai.model.User;
 import io.javalin.Javalin;
 
-import java.io.InputStream;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
+import io.javalin.rendering.template.JavalinJte;
 
 public class Main {
     static Connection conn;
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(
+                config -> { config.fileRenderer(new JavalinJte()); }
+        ).start(7000);
+
         final String url = "jdbc:postgresql://localhost:5432/bdr?currentSchema=embedded";
         final Properties props = new Properties();
         props.setProperty("user", "bdr");
@@ -22,6 +24,7 @@ public class Main {
             conn = DriverManager.getConnection(url, props);
             // Controllers
             UsersController usersController = new UsersController(conn);
+            BenefitsController benefitsController = new BenefitsController(conn);
 
             // Users routes
             app.get("/api/users",usersController::getAll);
@@ -29,7 +32,8 @@ public class Main {
             app.get("/api/users/{id}", usersController::getOne);
             app.put("/api/users/{id}", usersController::update);
             app.delete("/api/users/{id}", usersController::delete);
-            app.start(7000);
+
+            app.get("/benefits/{id}", benefitsController::getOne);
 
         } catch(SQLException e) {
             System.out.println("Error connecting to database " + Arrays.toString(e.getStackTrace()));
